c6871b057a22f085175780c143da9ea2564543bb
Ahmad Abdur Rahman Khan <163046609+ahmadkhan100@users.noreply.github.com>
2024-09-08 20:25:22 -0400

    Create __init__.py

---
+++ tests/__init__.py
@@ -0,0 +1,4 @@
+# tests/__init__.py
+
+# This file ensures the directory is treated as a Python package
+# It can be left empty or used for setup code for your tests, such as test suite configuration.
